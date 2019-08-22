6388aff863966144e3783083d40cd4ded2d2f478
Vikas Pandey <vikaspa6@gmail.com>
2019-08-22 12:34:13 +0530

    Update vikiscripts.ps.funtions.ps1

--- vikiscripts.ps.funtions.ps1
+++ vikiscripts.ps.funtions.ps1
@@ -3,35 +3,35 @@
 #-------------------------
 Function Get-NetworkStatistics 
 { 
-    $properties = ‘Protocol’,’LocalAddress’,’LocalPort’ 
-    $properties += ‘RemoteAddress’,’RemotePort’,’State’,’ProcessName’,’PID’
+    $properties = 'Protocol','LocalAddress','LocalPort' 
+    $properties += 'RemoteAddress','RemotePort','State','ProcessName','PID'
 
     netstat -ano | Select-String -Pattern ‘\s+(TCP|UDP)’ | ForEach-Object {
 
-        $item = $_.line.split(” “,[System.StringSplitOptions]::RemoveEmptyEntries)
+        $item = $_.line.split(" ",[System.StringSplitOptions]::RemoveEmptyEntries)
 
-        if($item[1] -notmatch ‘^\[::’) 
+        if($item[1] -notmatch '^\[::') 
         {            
-            if (($la = $item[1] -as [ipaddress]).AddressFamily -eq ‘InterNetworkV6’) 
+            if (($la = $item[1] -as [ipaddress]).AddressFamily -eq 'InterNetworkV6') 
             { 
                $localAddress = $la.IPAddressToString 
-               $localPort = $item[1].split(‘\]:’)[-1] 
+               $localPort = $item[1].split('\]:')[-1] 
             } 
             else 
             { 
-                $localAddress = $item[1].split(‘:’)[0] 
-                $localPort = $item[1].split(‘:’)[-1] 
+                $localAddress = $item[1].split(':')[0] 
+                $localPort = $item[1].split(':')[-1] 
             } 
 
-            if (($ra = $item[2] -as [ipaddress]).AddressFamily -eq ‘InterNetworkV6’) 
+            if (($ra = $item[2] -as [ipaddress]).AddressFamily -eq 'InterNetworkV6') 
             { 
                $remoteAddress = $ra.IPAddressToString 
-               $remotePort = $item[2].split(‘\]:’)[-1] 
+               $remotePort = $item[2].split('\]:')[-1] 
             } 
             else 
             { 
-               $remoteAddress = $item[2].split(‘:’)[0] 
-               $remotePort = $item[2].split(‘:’)[-1] 
+               $remoteAddress = $item[2].split(':')[0] 
+               $remotePort = $item[2].split(':')[-1] 
             } 
 
             New-Object PSObject -Property @{ 
@@ -42,7 +42,7 @@ Function Get-NetworkStatistics
                 LocalPort = $localPort 
                 RemoteAddress =$remoteAddress 
                 RemotePort = $remotePort 
-                State = if($item[0] -eq ‘tcp’) {$item[3]} else {$null} 
+                State = if($item[0] -eq 'tcp') {$item[3]} else {$null} 
             } | Select-Object -Property $properties 
         } 
     } 
