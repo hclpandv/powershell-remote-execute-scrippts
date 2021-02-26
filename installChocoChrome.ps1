a49178888bc38c34caa26379838d803dacdb8b3e
Vikas Pandey <vikaspa6@gmail.com>
2021-02-26 15:06:27 +0800

    Update installChocoChrome.ps1

--- installChocoChrome.ps1
+++ installChocoChrome.ps1
@@ -3,6 +3,8 @@
 #    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://bit.ly/3ssg5jv'))
 #---------------------------------------------
 
+## Help : https://markheath.net/post/create-configure-vm-azure-cli
+
 # Insall ChocoLatey
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
