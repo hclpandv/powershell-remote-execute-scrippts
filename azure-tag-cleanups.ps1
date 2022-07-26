bbb98fce05321ea91935ad33155a6cc5f01963b4
Vikas Pandey <vikas110@gmail.com>
2022-07-27 06:56:26 +0800

    Update azure-tag-cleanups.ps1

--- azure-tag-cleanups.ps1
+++ azure-tag-cleanups.ps1
@@ -23,3 +23,38 @@ Function Repair-AzureTags($oldKey,$newKey){
 }
 
 Repair-AzureTags -oldKey Application_Owner -newKey ApplicationOwner
+
+
+Function Repair-AzureTags(){
+<#
+ .SYNOPSIS
+    Repairs the Tag Keys on multiple resources simnultaneously.
+#>
+    [CmdletBinding(DefaultParameterSetName='SubscriptionScope')]
+    param(
+      [parameter(Mandatory=$true)]
+      [string]$oldKey,
+      [parameter(Mandatory=$true)]
+      [string]$newKey,
+      [parameter(ParameterSetName='RgScope')]
+      [switch]$scopeToResourceGroup,
+      [parameter(ParameterSetName='RgScope')]
+      [string]$ressourceGroupName
+      
+    )
+    $targets_resources = Get-AzResource | Where-Object{$_.Tags.Keys -match $oldKey} 
+    #$targets_resources | Out-File -FilePath 
+    $targets_resources | ForEach-Object {
+        $OldKeyValue = $_.Tags.$oldKey
+        $NewTag = @{$newKey=$OldKeyValue}
+        $OldTag = @{$oldKey=$OldKeyValue}
+        $resourceID = $_.ResourceId
+        Update-AzTag -ResourceId $resourceID -Tag $NewTag -Operation Merge
+        $Check = Get-AzResource -Id $resourceID | Where-Object {$_.Tags.Keys -match $newKey}
+        if ($Check) {
+            Update-AzTag -ResourceId $resourceID -Tag $OldTag -Operation Delete
+        }
+    }
+}
+
+
