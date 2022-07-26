0801880229fd70571ad66b1b071b630a9b0dcd77
Vikas Pandey <vikas110@gmail.com>
2022-07-26 20:43:12 +0800

    Update azure-tag-cleanups.ps1

--- azure-tag-cleanups.ps1
+++ azure-tag-cleanups.ps1
@@ -5,3 +5,21 @@ Function Repair-AzureTags($resourceName,$resourceGroupName,$badKey,$correctKey){
    $target.Tags.$correctKey = $previousValue
    $target | Set-AzResource -Tag $target.Tags -Force
 }
+
+
+Function Repair-AzureTags($oldKey,$newKey){
+    $targets_resources = Get-AzResource | Where-Object{$_.Tags.Keys -match $oldKey} 
+    $targets_resources | ForEach-Object {
+        $OldKeyValue = $_.Tags.$oldKey
+        $NewTag = @{$newKey=$OldKeyValue}
+        $OldTag = @{$oldKey=$OldKeyValue}
+        $resourceID = $_.ResourceId
+        Update-AzTag -ResourceId $resourceID -Tag $NewTag -Operation Merge
+        $Check = Get-AzResource -Id $resourceID | Where-Object {$_.Tags.Keys -match $newKey}
+        if ($Check) {
+            Update-AzTag -ResourceId $resourceID -Tag $OldTag -Operation Delete
+        }
+    }
+}
+
+Repair-AzureTags -oldKey Application_Owner -newKey ApplicationOwner
