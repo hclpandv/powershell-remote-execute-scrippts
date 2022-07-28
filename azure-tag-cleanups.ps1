a7c94c501492899cd95a3108c3f6816c3a492dd3
Vikas Pandey <vikas110@gmail.com>
2022-07-28 22:59:14 +0800

    Update azure-tag-cleanups.ps1

--- azure-tag-cleanups.ps1
+++ azure-tag-cleanups.ps1
@@ -25,36 +25,22 @@ Function Repair-AzureTags($oldKey,$newKey){
 Repair-AzureTags -oldKey Application_Owner -newKey ApplicationOwner
 
 
-Function Repair-AzureTags(){
-<#
- .SYNOPSIS
-    Repairs the Tag Keys on multiple resources simnultaneously.
-#>
-    [CmdletBinding(DefaultParameterSetName='SubscriptionScope')]
-    param(
-      [parameter(Mandatory=$true)]
-      [string]$oldKey,
-      [parameter(Mandatory=$true)]
-      [string]$newKey,
-      [parameter(ParameterSetName='RgScope')]
-      [switch]$scopeToResourceGroup,
-      [parameter(ParameterSetName='RgScope')]
-      [string]$ressourceGroupName
-      
-    )
-    $targets_resources = Get-AzResource | Where-Object{$_.Tags.Keys -match $oldKey} 
-    #$targets_resources | Out-File -FilePath 
-    $targets_resources | ForEach-Object {
-        $OldKeyValue = $_.Tags.$oldKey
-        $NewTag = @{$newKey=$OldKeyValue}
-        $OldTag = @{$oldKey=$OldKeyValue}
-        $resourceID = $_.ResourceId
-        Update-AzTag -ResourceId $resourceID -Tag $NewTag -Operation Merge
-        $Check = Get-AzResource -Id $resourceID | Where-Object {$_.Tags.Keys -match $newKey}
-        if ($Check) {
-            Update-AzTag -ResourceId $resourceID -Tag $OldTag -Operation Delete
-        }
-    }
+Function Rename-AzureTags(){
+   [CmdletBinding()]
+   param(
+     [parameter(Mandatory=$true,ValueFromPipeline=$true)]
+     $target,
+     [string]$oldKey,
+     [string]$newKey   
+   )
+
+   #$target = Get-AzResource -ResourceGroupName $resourceGroupName -Name $resourceName
+   $previousValue = $target.Tags.$oldKey
+   $target.Tags.Remove($oldKey)
+   $target.Tags.$newKey = $previousValue
+   $target | Set-AzResource -Tag $target.Tags -Force
 }
 
+
+Get-AzResource -resourceName tfstate1395347833 -resourceGroupName TerraformStateRG | %{ Rename-AzureTags -target $_  -oldKey "Correct_Application_Owner" -newKey "Application_Owner"}
 
